c8eea323e87c3a960714c09b4ede57b9942a74ee
Mike Bluestein <mike.bluestein@gmail.com>
2013-11-06 10:35:26 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+SnapBehaviorDemo
+================
+
+This is a small application that demonstrates how to use UIKitDynamics to snap an image into place.
+License:
+
+This code is licensed under the terms of the MIT/X11 license.
---
+++ SnapBehaviorDemo/SnapBehaviorDemoViewController.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using MonoTouch.CoreGraphics;
+
+namespace SnapBehaviorDemo
+{
+	public class SnapBehaviorDemoViewController : UIViewController
+	{
+		UIPanGestureRecognizer panGesture;
+		UIImageView imageView;
+		UIDynamicAnimator animator;
+		UISnapBehavior snap;
+		UIView snapArea;
+		RectangleF snapRect;
+		PointF snapPoint;
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			AddSnapArea ();
+
+			animator = new UIDynamicAnimator (View);
+
+			float dx = 0;
+			float dy = 0;
+
+			using (var image = UIImage.FromFile ("monkey.png")) {
+				imageView = new UIImageView (image){ Frame = new RectangleF (new PointF (0, 0), image.Size) };
+				imageView.UserInteractionEnabled = true;
+				View.AddSubview (imageView);
+			}
+
+			panGesture = new UIPanGestureRecognizer ((pg) => {
+				if ((pg.State == UIGestureRecognizerState.Began || pg.State == UIGestureRecognizerState.Changed) && (pg.NumberOfTouches == 1)) {
+
+					if (snap != null)
+						animator.RemoveBehavior (snap);
+
+					var p0 = pg.LocationInView (View);
+
+					if (dx == 0)
+						dx = p0.X - imageView.Center.X;
+
+					if (dy == 0)
+						dy = p0.Y - imageView.Center.Y;
+
+					var p1 = new PointF (p0.X - dx, p0.Y - dy);
+
+					imageView.Center = p1;
+				} else if (pg.State == UIGestureRecognizerState.Ended) {
+					dx = 0;
+					dy = 0;
+
+					SnapImageIntoPlace (pg.LocationInView (View));
+				}
+			});
+
+			imageView.AddGestureRecognizer (panGesture);
+
+			View.BackgroundColor = UIColor.White;
+		}
+
+		void SnapImageIntoPlace (PointF touchPoint)
+		{
+			if (snapRect.Contains (touchPoint)) {
+				if (snap != null)
+					animator.RemoveBehavior (snap);
+
+				snap = new UISnapBehavior (imageView, snapPoint);
+				animator.AddBehavior (snap);
+			}
+		}
+
+		void AddSnapArea ()
+		{
+			snapRect = new RectangleF (View.Center.X - 100, View.Center.Y - 100, 200, 200);
+			snapPoint = new PointF (snapRect.GetMidX (), snapRect.GetMidY ());
+
+			snapArea = new UIView (snapRect) {
+				BackgroundColor = UIColor.LightGray
+			};
+
+			View.AddSubview (snapArea);
+		}
+	}
+}
+
+
+
